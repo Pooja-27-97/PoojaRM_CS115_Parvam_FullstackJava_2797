7c725cb2a84b442ee919e875dc1c5dbbea057539
Pooja-27-97 <poojarm2797@gmail.com>
2025-09-24 01:25:31 +0530

    Day-2 files

--- mysql/day2/assignment.sql
+++ mysql/day2/assignment.sql
@@ -16,30 +16,70 @@ values
 ("Alice", "pen-5, pencil-3, book-2", "Mumbai");
 select * from orders_unf;
 
-create table cust_order(
-	cust_id int primary key auto_increment,
-    cust_name varchar(30) not null,
-    cust_city varchar(30) not null
+CREATE TABLE orders_1nf (
+    order_id INT,
+    cust_name VARCHAR(50),
+    item_name VARCHAR(50),
+    quantity INT,
+    cust_city VARCHAR(50)
 );
-insert into cust_order(cust_name, cust_city)
-values
-("Alice","Mumbai"),
-("Bob","Delhi");
+INSERT INTO orders_1nf VALUES
+(1, 'Alice', 'pen', 2, 'Mumbai'),
+(1, 'Alice', 'book', 1, 'Mumbai'),
+(2, 'Bob', 'book', 3, 'Delhi'),
+(3, 'Alice', 'pen', 5, 'Mumbai'),
+(3, 'Alice', 'pencil', 3, 'Mumbai'),
+(3, 'Alice', 'book', 2, 'Mumbai');
+select * from orders_1nf;
 
 
-drop table item_quantity;
-create table item_quantity(
-	cust_id int primary key,
-    item_name varchar(20) not null,
-    item_quant int
+-- Customers Table
+CREATE TABLE customers (
+    cust_id INT PRIMARY KEY AUTO_INCREMENT,
+    cust_name VARCHAR(50),
+    cust_city VARCHAR(50)
 );
+INSERT INTO customers VALUES
+(1, 'Alice', 'Mumbai'),
+(2, 'Bob', 'Delhi');
+select * from customers;
 
-insert into item_quantity(cust_id, item_name, item_quant)
-values
-(1,"pen",2),
-(1,"book",1),
-(2,"book",3),
-(3,"pen",5),
-(3,"pencil", 3),
-(3,"book", 2);
-select * from item_quantity;
+-- Orders Table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    cust_id INT,
+    FOREIGN KEY (cust_id) REFERENCES customers(cust_id)
+);
+INSERT INTO orders VALUES
+(1, 1),
+(2, 2),
+(3, 1);
+select * from orders;
+
+-- Order Items Table
+CREATE TABLE order_items (
+    order_id INT,
+    item_name VARCHAR(50),
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+INSERT INTO order_items VALUES
+(1, 'pen', 2),
+(1, 'book', 1),
+(2, 'book', 3),
+(3, 'pen', 5),
+(3, 'pencil', 3),
+(3, 'book', 2);
+select * from order_items;
+
+
+-- Items Table
+CREATE TABLE items (
+    item_name VARCHAR(50) PRIMARY KEY,
+    price DECIMAL(10,2)
+);
+INSERT INTO items VALUES
+('pen', 10.00),
+('book', 100.00),
+('pencil', 5.00);
+select * from  items;
